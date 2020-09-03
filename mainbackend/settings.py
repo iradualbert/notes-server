8a839581997cfbc357fa3c1294fa1322226f7fc3
Albert Iradukunda <albert.iradukunda@yahoo.com>
2020-09-03 21:31:57 +0300

    static files

--- mainbackend/settings.py
+++ mainbackend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,6 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 CORS_ALLOW_METHODS = [
     'DELETE',
